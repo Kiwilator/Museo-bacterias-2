39707f6fd7baf5ad1d1aed1183f19eca7ee807f4
Kiwilator <so.revuelta@gmail.com>
2026-09-16 08:22:51 +0200

    Center sponsor sign further toward purple side

--- script.js
+++ script.js
@@ -1348,7 +1348,7 @@ AFRAME.registerComponent('sponsor-wall-anchor', {
       const target = new THREE.Vector3(
         (2 * roomCenter.x) - sourceSide.x,
         spawn.y + 1.54,
-        THREE.MathUtils.lerp(sourceSide.z, palCenter.z, 1.45)
+        THREE.MathUtils.lerp(sourceSide.z, palCenter.z, 1.78)
       );
 
       // Snap it to the actual wall surface on that side, then pull it a few
